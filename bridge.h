1d7060f2a8ede55d4a3c191c5a71f735ee650dcc
Richard Kmieciak <mr.kmieciak@gmail.com>
2019-07-04 11:05:59 -0700

    adding amplitude sdk

---
+++ bridge.h
@@ -0,0 +1,13 @@
+//
+//  bridge.h
+//  ios_analytics_poc
+//
+//  Created by Richard Kmieciak on 7/4/19.
+//  Copyright © 2019 Richard Kmieciak. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+
+#endif /* bridge_h */
--- ios_analytics_poc/AppDelegate.swift
+++ ios_analytics_poc/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Amplitude_iOS
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
