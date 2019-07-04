50b21d4d94ce00adb0e8cf4b14807c05cfd3c0e6
Richard Kmieciak <mr.kmieciak@gmail.com>
2019-07-04 09:41:35 -0700

    Initial Commit

---
+++ ios_analytics_poc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_analytics_poc
+//
+//  Created by Richard Kmieciak on 7/4/19.
+//  Copyright © 2019 Richard Kmieciak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
